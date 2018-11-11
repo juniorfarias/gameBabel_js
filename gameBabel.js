b4e9f7fe1503456e141b59635430e58a51233c50
JuniorFarias <junior13farias@gmail.com>
2018-11-11 14:52:45 -0300

    ReFinal

--- gameBabel.js
+++ gameBabel.js
@@ -1,8 +1,8 @@
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 512;
-canvas.height = 480;
+canvas.width = 256;
+canvas.height = 256;
 document.body.appendChild(canvas);
 
 let bgReady = false;
@@ -43,11 +43,11 @@ window.addEventListener('keyup', function (e) {
 }, false);
 
 const reset = function () {
-    hero.x = canvas.width / 2
-    hero.y = canvas.height / 2
+    hero.x = canvas.width / 8
+    hero.y = canvas.height / 8
 
-    monster.x = 32 + (Math.random() * (canvas.width - 64))
-    monster.y = 32 + (Math.random() * (canvas.width - 64))
+    monster.x = 16 + (Math.random() * (canvas.width - 32))
+    monster.y = 16 + (Math.random() * (canvas.width - 32))
 }
 
 const update = function (modifier) {
@@ -63,12 +63,50 @@ const update = function (modifier) {
     if (39 in keysDown) {//Direita
         hero.y += hero.speed * modifier
     }
+    
+    if (
+        hero.x <= (monster.x + 16) && monster.x <= (hero.x + 16) 
+        && hero.y <= (monster.y + 16) && monster.y <= (hero.y + 16)
+    ) {
+        ++monstersCaught
+        reset()
+    }
 }
 
-if (
-    hero.x <= (monster.x + 32) && monster.x <= (hero.x + 32) 
-    && hero.y <= (monster.y + 32) && monster.y <= (hero.y + 32)
-) {
-    ++monstersCaught
-    reset()
+const render = function () {
+    if (bgReady) {
+        ctx.drawImage(bgImage, 0, 0)
+    }
+    if (heroReady) {
+        ctx.drawImage(heroImage, hero.x, hero.y)
+    }
+    if (monsterReady) {
+        ctx.drawImage(monsterImage, monster.x, monster.y)
+    }
+
+    ctx.fillStyle = 'rgb(0, 0, 0)'
+    ctx.font = '16px Helvetica'
+    ctx.textAlign = 'left'
+    ctx.textBaseline = 'top'
+    ctx.fillText('Monstros pegos: ' + monstersCaught, 32, 32)
 }
+
+//Loop do jogo em sí
+const main = function () {
+    const now = Date.now()
+    const delta = now - then
+
+    update(delta / 1000)
+    render()
+
+    then = now
+
+    requestAnimationFrame(main)
+}
+
+const w = window
+const requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame
+
+let then = Date.now()
+reset()
+main()
